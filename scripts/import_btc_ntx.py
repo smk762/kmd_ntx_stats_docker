ac5bdea71e95a80a07149edcbfaaed46e016f593
smk762 <smk762@iinet.net.au>
2020-06-30 17:35:07 +0200

    next page

--- scripts/import_btc_ntx.py
+++ scripts/import_btc_ntx.py
@@ -29,28 +29,47 @@ logger.setLevel(logging.INFO)
 
 load_dotenv()
 
-def import_btc_ntx():
+def import_btc_ntx(existing_txids):
     r = requests.get("http://notary.earth:8762/api/source/notarised/?season=Season_4&chain=BTC")
     resp = r.json()
     while resp["next"] != '':
-        results = resp["results"]
 
+        results = resp["results"]
+        next_page = resp["next"]
         for item in results:
-            print(item)
+            if item["txid"] not in existing_txids:
+                row_data = (item["chain"], item["block_height"], item["block_time"],
+                            item["block_datetime"], item["block_hash"],
+                            item["notaries"], item["ac_ntx_blockhash"], item["ac_ntx_height"],
+                            item["txid"], item["opret"], item["season"], "true")
+
+                update_ntx_row(conn, cursor, row_data)
+                logger.info("Updated "+item['txid'])
+            else:
+                logger.info("Skipping "+item['txid'])
+
+        r = requests.get(resp["next"])
+        resp = r.json()
+
+    results = resp["results"]
+    for item in results:
+        if item["txid"] not in existing_txids:
             row_data = (item["chain"], item["block_height"], item["block_time"],
                         item["block_datetime"], item["block_hash"],
                         item["notaries"], item["ac_ntx_blockhash"], item["ac_ntx_height"],
                         item["txid"], item["opret"], item["season"], "true")
 
             update_ntx_row(conn, cursor, row_data)
-
-            time.sleep(1)
-
+            logger.info("Updated "+item['txid'])
+        else:
+            logger.info("Skipping "+item['txid'])
 
 conn = connect_db()
 cursor = conn.cursor()
 
-import_btc_ntx()
+existing_txids = get_exisiting_btc_ntxids(cursor)
+
+import_btc_ntx(existing_txids)
 
 cursor.close()
 conn.close()
