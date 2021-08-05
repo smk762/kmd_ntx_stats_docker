c038cb83e7a5486b61305aecf34761832672fbde
smk762 <smk762@iinet.net.au>
2021-08-05 14:40:32 +0200

    hint

--- code/scripts/cron_populate_last_ntx_table.py
+++ code/scripts/cron_populate_last_ntx_table.py
@@ -18,7 +18,9 @@ from models import  last_notarised_row
 def update_latest_ntx(season):
 
     chains = get_notarised_chains(season)
+    print(chains)
     season_last_ntx = get_season_last_ntx(season)
+    print(season_last_ntx)
     notaries = get_season_notaries(season)
     for chain in chains:
         server = get_chain_server(chain, season)
@@ -38,51 +40,40 @@ def update_latest_ntx(season):
                    AND chain = '"+str(chain)+"' \
                    AND notaries @> "+"'{\""+notary+"\"}'";
 
-            try:
-                #logger.info(f"[update_latest_ntx] {sql}")
-                CURSOR.execute(sql)
-                resp = CURSOR.fetchone()
-                block_height = resp[0]
-                if block_height:
-                    if block_height > season_last_ntx[notary][chain]:
-                        sql = "SELECT block_time, txid \
-                               FROM notarised \
-                               WHERE chain='"+str(chain)+"' \
-                               AND block_height="+str(block_height)+";"
+            CURSOR.execute(sql)
+            resp = CURSOR.fetchone()
+            block_height = resp[0]
+            if block_height:
+                if block_height > season_last_ntx[notary][chain]:
+                    sql = "SELECT block_time, txid \
+                           FROM notarised \
+                           WHERE chain='"+str(chain)+"' \
+                           AND block_height="+str(block_height)+";"
 
-                        try:
-                            logger.info(f"[update_latest_ntx] {sql}")
-                            CURSOR.execute(sql)
-                            x = CURSOR.fetchone()
-                            block_time = x[0]
-                            txid = x[1]
-                            row = last_notarised_row()
-                            row.notary = notary
-                            row.chain = chain
-                            row.txid = txid
-                            row.block_height = block_height
-                            row.block_time = block_time
-                            row.season = season
-                            row.server = server
-                            row.update()
-
-                        except Exception as e:
-                            logger.error(f"Exception in [update_latest_ntx] where block_time: {e}")
-                else:
+                    logger.info(f"[update_latest_ntx] {sql}")
+                    CURSOR.execute(sql)
+                    x = CURSOR.fetchone()
+                    block_time = x[0]
+                    txid = x[1]
                     row = last_notarised_row()
                     row.notary = notary
                     row.chain = chain
-                    row.txid = "N/A"
-                    row.block_height = 0
-                    row.block_time = 0
+                    row.txid = txid
+                    row.block_height = block_height
+                    row.block_time = block_time
                     row.season = season
                     row.server = server
                     row.update()
-
-
-
-            except Exception as e:
-                logger.error(f"Exception in [update_latest_ntx] max block_time: {e}")
+            else:
+                row = last_notarised_row()
+                row.notary = notary
+                row.chain = chain
+                row.txid = "N/A"
+                row.block_height = 0
+                row.block_time = 0
+                row.season = season
+                row.server = server
+                row.update()
 
 
 if __name__ == "__main__":
