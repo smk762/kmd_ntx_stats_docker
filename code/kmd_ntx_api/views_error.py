777e86404c48f79dc228af14093ab6e3304fe685
smk762 <smk762@iinet.net.au>
2021-12-05 16:30:23 +0100

    err pgs

--- code/kmd_ntx_api/views_error.py
+++ code/kmd_ntx_api/views_error.py
@@ -1,31 +1,52 @@
 from django.shortcuts import render
 
+def get_base_context(request):
+    season = get_page_season(request)
+    scheme_host = get_current_host(request)
+    context = {
+        "season":season,
+        "scheme_host": scheme_host,
+        "sidebar_links":get_sidebar_links(season),
+        "eco_data_link":get_eco_data_link()
+    }
+
+
 def error_400(request,  exception):
-        context = {
-        	"error":"400 - Bad Request"
-        }
-        return render(request,'error/generic_error.html', context)
+    context = get_base_context(request)
+    context.update({
+        "error":"400 - Bad Request"
+    })
+    return render(request,'error/generic_error.html', context)
+
 
 def error_403(request, exception):
-        context = {
-        	"error":"403 - Verboten"
-        }
-        return render(request,'error/generic_error.html', context)
+    context = get_base_context(request)
+    context.update({
+        "error":"403 - Verboten"
+    })
+    return render(request,'error/generic_error.html', context)
+
 
 def error_404(request, exception):
-        context = {
-        	"error":"404 - Not Found"
-        }
-        return render(request,'error/generic_error.html', context)
+    context = get_base_context(request)
+    context.update({
+        "error":"404 - Not Found"
+    })
+    return render(request,'error/generic_error.html', context)
+
 
 def error_500(request):
-        context = {
-        	"error":"500 - Internal Server Error"
-        }
-        return render(request,'error/generic_error.html', context)
+    context = get_base_context(request)
+    context.update({
+        "error":"500 - Internal Server Error"
+    })
+    return render(request,'error/generic_error.html', context)
+    
 
 def error_502(request, exception):
-        context = {
-        	"error":"502 - Naughty Gateway"
-        }
-        return render(request,'error/generic_error.html', context)
+    context = get_base_context(request)
+    context.update({
+        "error":"502 - Naughty Gateway"
+    })
+    return render(request,'error/generic_error.html', context)
+    
