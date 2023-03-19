00a8c0e6409d92f7f167d52ec8fb57ff624078f9
smk762 <smk762@iinet.net.au>
2023-03-19 07:56:47 +0100

    -fix coins data sourcing

--- code/scripts/lib_coins.py
+++ code/scripts/lib_coins.py
@@ -33,110 +33,161 @@ def get_assetchain_cli(ac_name):
     return f"~/komodo/src/komodo-cli -ac_name={ac_name}"
 
 
-def get_coins_repo_lightwallets(lightwallets, coins_data):
-    for coin in coins_data:
-        data = None
-        _coin = get_alt_name(coin)
-        if _coin in lightwallets:
-            data = requests.get(lightwallets[_coin]).json()
-        elif coin in lightwallets:
-            data = requests.get(lightwallets[coin]).json()
-        if data:
-            logger.info(f"[get_coins_repo_lightwallets] {coin} {data}")
-            coins_data[coin]['lightwallets'] = data
-        else:
-            pass
-            # logger.warning(f"[get_coins_repo_lightwallets] No electrum for {coin}")
+@print_runtime
+def parse_coins_repo():
+    '''
+    Gets data from coins repository and prepopulates the coins_data dict.
+    Some coins are named differently in dPoW Repo (e.g. ARRR/TKL) so
+    in thiese cases both are initialised.
+    '''
+    coins_data = {}
+    r = requests.get(get_coins_repo_coins_url())
+    coins_repo = r.json()
 
+    for item in coins_repo:
+        coin = item['coin']
+        _coin = get_alt_name(item['coin'])
+        if coin != _coin:
+            coins_data = get_coins_repo_info(coins_data, _coin, item)
+        coins_data = get_coins_repo_info(coins_data, coin, item)
+    return coins_data
+
+
+def get_coins_repo_info(coins_data, coin, item):
+    ''' Add coins repository data for a coin to the coins_data dict'''
+    logger.info(f"[get_coins_repo_info] Getting info for {coin}")
+    coins_data = pre_populate_coins_data(coins_data, coin)
+
+    if "derivation_path" in item:
+        item["derivation_path"] = item["derivation_path"].replace("'", "-")
+
+    coins_data[coin].update({"coins_info":item})
+
+    if 'asset' in item:
+        coins_data[coin]["coins_info"].update(SMARTCHAIN_BASE_58)
+
+    if "mm2" not in coins_data[coin]["coins_info"]:
+        coins_data[coin]["coins_info"].update({"mm2": 0})
+
+    coins_data[coin].update({
+                "mm2_compatible": coins_data[coin]["coins_info"]["mm2"]
+            })
+    return coins_data
+
+
+@print_runtime
+def parse_dpow_coins(coins_data):
+    '''
+    Gets data from dPoW repo to use for stats.
+    No coin renaming or name duplication for this data.
+    '''
+    r = requests.get(get_dpow_readme_url())
+    dpow_readme = r.text
+    lines = dpow_readme.splitlines()
+
+    for line in lines:
+        info = [i.strip() for i in line.split("|")]
+
+        if len(info) > 4 and info[0].lower() not in ['coin', '--------']:
+
+            coins_data[coin].update({
+                "dpow":{
+                    "src": get_dpow_coin_src(info[1]),
+                    "version": info[2],
+                    "server": get_dpow_coin_server(info[4])
+                },
+                "dpow_active":1
+            })
+
+    return coins_data
+
+
+def get_coins_repo_lightwallets(lightwallets, coins_data):
+    '''
+    Gets lightwallet data from coins repo to use for activation commands.
+    No coin renaming or name duplication
+    '''
+    for coin in lightwallets:
+        data = requests.get(lightwallets[coin]).json()
+        logger.info(f"[get_coins_repo_lightwallets] {coin}")
+        coins_data[coin]['lightwallets'] = data
     return coins_data
 
 
 def get_coins_repo_electrums(electrums, coins_data):
-
-    for coin in coins_data:
-        data = None
-        _coin = get_alt_name(coin)
-        if _coin in electrums:
-            data = requests.get(electrums[_coin]).json()
-        elif coin in electrums:
-            data = requests.get(electrums[coin]).json()
-        if data:
-            logger.info(f"[get_coins_repo_explorers] {coin} {data[0]}")
-            for item in data:
-                if "protocol" in item:
-                    if item['protocol'] == "SSL":
-                        coins_data[coin]['electrums_ssl'].append(item['url'])
-                    else:
-                        coins_data[coin]['electrums'].append(item['url'])
+    '''
+    Gets electrum data from coins repo to use for activation commands.
+    No coin renaming or name duplication
+    '''
+    for coin in electrums:
+        coins_data = pre_populate_coins_data(coins_data, coin)
+        data = requests.get(electrums[coin]).json()
+        logger.info(f"[get_coins_repo_electrums] {coin}")
+        for item in data:
+            if "protocol" in item:
+                if item['protocol'] == "SSL":
+                    coins_data[coin]['electrums_ssl'].append(item['url'])
                 else:
                     coins_data[coin]['electrums'].append(item['url'])
-        else:
-            pass
-            # logger.warning(f"[get_coins_repo_explorers] No electrum for {coin}")
-
+            else:
+                coins_data[coin]['electrums'].append(item['url'])
     return coins_data
 
 
 def get_coins_repo_explorers(explorers, coins_data):
-    for coin in coins_data:
-        data = None
-        _coin = get_alt_name(coin)
-        if _coin in explorers:
-            data = requests.get(explorers[_coin]).json()
-        elif coin in explorers:
-            data = requests.get(explorers[coin]).json()
-        if data:
-            # logger.info(f"[get_coins_repo_explorers] {coin} {data}")
-            for explorer in data:
-                coins_data[coin]['explorers'].append(explorer.replace("/tx/", "/").replace("/tx.dws?", "/"))
-        else:
-            pass
-            # logger.warning(f"[get_coins_repo_explorers] No explorers for {coin}")
+    '''
+    Gets explorer data from coins repo to use for activation commands.
+    No coin renaming or name duplication
+    '''
+    for coin in explorers:
+        pre_populate_coins_data(coins_data, coin)
+        data = requests.get(explorers[coin]).json()
+        logger.info(f"[get_coins_repo_explorers] {coin}")
+        for explorer in data:
+            coins_data[coin]['explorers'].append(explorer.replace("/tx/", "/").replace("/tx.dws?", "/"))
              
     return coins_data
 
 
-def get_coins_repo_icons(icons, coins_data):
-    logger.info(icons)
-    for coin in coins_data:
-        data = None
-        _coin = get_alt_name(coin)
+def drop_suffix(coin):
+    if coin.endswith("-AVX20") or coin.endswith("-BEP20") or coin.endswith("-ERC20")\
+                                 or coin.endswith("-HRC20") or coin.endswith("-KRC20")\
+                                 or coin.endswith("-PLG20") or coin.endswith("-FTM20")\
+                                 or coin.endswith("-HCO20") or coin.endswith("-OLD")\
+                                 or coin.endswith("-MVR20") or coin.endswith("-segwit"):
+        return coin.split("-")[0]
+    else:
+        return coin
 
-        if coin == "GLEEC-OLD":
-            _coin = "GLEEC"
-        elif coin.endswith("-AVX20") or coin.endswith("-BEP20") or coin.endswith("-ERC20")\
-                                     or coin.endswith("-HRC20") or coin.endswith("-KRC20")\
-                                     or coin.endswith("-PLG20") or coin.endswith("-FTM20")\
-                                     or coin.endswith("-HCO20") or coin.endswith("-OLD")\
-                                     or coin.endswith("-MVR20") or coin.endswith("-segwit"):
-            _coin = coin.split("-")[0]
-        else:
-            _coin = coin
+
+def get_coins_repo_icons(icons, coins_data):
+    '''
+    Gets coin icon path from coins repo.
+    No coin renaming or name duplication
+    '''
+    for coin in coins_data:
+        _coin = drop_suffix(coin)
+        alt_name = get_alt_name(coin)
 
         if f"{_coin.lower()}.png" in icons:
-            data = icons[f"{_coin.lower()}.png"]
-        elif f"{coin.lower()}.png" in icons:
-            data = icons[f"{coin.lower()}.png"]
-
-        if data:
-            logger.info(f"[get_coins_repo_icons] {coin} {data}")
-            coins_data[coin]["coins_info"].update({"icon": data})
-        else:
-            logger.warning(f"[get_coins_repo_icons] No icon for {coin}")
-            pass
+            coins_data[coin]["coins_info"].update({"icon": icons[f"{_coin.lower()}.png"]})
+        if f"{alt_name.lower()}.png" in icons:
+            coins_data[coin]["coins_info"].update({"icon": icons[f"{alt_name.lower()}.png"]})
 
     return coins_data
 
 
 @print_runtime
 def get_dpow_tenure(coins_data):
-
+    '''
+    Gets dPoW tenure data from database.
+    No coin renaming or name duplication.
+    '''
     now = int(time.time())
     notarised_coins = get_season_coins()
 
     for coin in notarised_coins:
-        coin = handle_translate_coins(coin)
-        coins_data = pre_populate_coins_data(coins_data, coin)
+
         url = get_notarised_tenure_table_url(None, None, coin)
         notarised_tenure = requests.get(url).json()["results"]
 
@@ -175,64 +226,40 @@ def get_dpow_tenure(coins_data):
 
     return coins_data
 
-@print_runtime
-def parse_dpow_coins(coins_data):
-    r = requests.get(get_dpow_readme_url())
-    dpow_readme = r.text
-    lines = dpow_readme.splitlines()
-
-    for line in lines:
-        info = [i.strip() for i in line.split("|")]
-
-        if len(info) > 4 and info[0].lower() not in ['coin', '--------']:
-
-            coin = handle_translate_coins(info[0])
-            logger.info(f"[parse_dpow_coins] Adding {coin} to dpow")
-            coins_data = pre_populate_coins_data(coins_data, coin)
-
-            coins_data[coin].update({
-                "dpow":{
-                    "src": get_dpow_coin_src(info[1]),
-                    "version": info[2],
-                    "server": get_dpow_coin_server(info[4])            
-                },
-                "dpow_active":1
-            })
-
-    return coins_data
-
 
 @print_runtime
 def parse_assetchains(coins_data):
+    '''
+    Gets launch/conf data for dPoW coins.
+    No coin renaming or name duplication
+    '''
     r = requests.get(get_dpow_assetchains_url())
     for item in r.json():
 
-        ac_name = item['ac_name']
-        coin = handle_translate_coins(ac_name)
-        coins_data = pre_populate_coins_data(coins_data, coin)
+        coin = item['ac_name']
 
         coins_data[coin]["coins_info"].update({
-            "cli": get_assetchain_cli(ac_name),
-            "conf_path": get_assetchain_conf_path(ac_name),
+            "cli": get_assetchain_cli(coin),
+            "conf_path": get_assetchain_conf_path(coin),
             "launch_params": get_assetchain_launch_params(item)
         })
 
     for coin in OTHER_CLI:
-        coins_data = pre_populate_coins_data(coins_data, coin)
         coins_data[coin]["coins_info"].update({"cli":OTHER_CLI[coin]})
 
     for coin in OTHER_CONF_FILE:
-        coins_data = pre_populate_coins_data(coins_data, coin)
         coins_data[coin]["coins_info"].update({"conf_path":OTHER_CONF_FILE[coin]})
 
     for coin in OTHER_LAUNCH_PARAMS:
-        coins_data = pre_populate_coins_data(coins_data, coin)
         coins_data[coin]["coins_info"].update({"launch_params":OTHER_LAUNCH_PARAMS[coin]})
 
     return coins_data
 
 
 def remove_delisted_coins():
+    '''
+    Removes old coins from database
+    '''
     r = requests.get(get_coins_repo_coins_url())
     coins_repo = r.json()
     coins_repo_coins = []
@@ -244,64 +271,37 @@ def remove_delisted_coins():
         delist_coin(coin)
 
 
-
-@print_runtime
-def parse_coins_repo():
-
-    coins_data = {}
-    r = requests.get(get_coins_repo_coins_url())
-    coins_repo = r.json()
-
-    for item in coins_repo:
-        coin = handle_translate_coins(item['coin'])
-        coins_data = pre_populate_coins_data(coins_data, coin)
-        item['coin'] = coin
-
-        logger.info(f"[parse_coins_repo] Getting info for {coin}")
-        if "derivation_path" in item:
-            item["derivation_path"] = item["derivation_path"].replace("'", "-")
-        coins_data.update({coin:{"coins_info":item}})
-
-        if 'asset' in item:
-            coins_data[coin]["coins_info"].update(SMARTCHAIN_BASE_58)
-
-        if "mm2" not in coins_data[coin]["coins_info"]:
-            coins_data[coin]["coins_info"].update({"mm2": 0})
-
-        coins_data[coin].update({
-                    "mm2_compatible": coins_data[coin]["coins_info"]["mm2"]
-                })
-    return coins_data
-
-
 @print_runtime
 def parse_electrum_explorer(coins_data):
+    '''
+    Gets supplemental coins repo info.
+    '''
     coins = list(coins_data.keys())
     coins.sort()
 
     explorers_data = get_github_folder_contents("KomodoPlatform", "coins", "explorers")
     explorers = {}
     for item in explorers_data:
-        explorers.update({item["name"]:item["download_url"]})
+        if item["name"] not in ["deprecated", "explorer_paths.json"]:
+            explorers.update({item["name"]:item["download_url"]})
 
     electrums_data = get_github_folder_contents("KomodoPlatform", "coins", "electrums")
     electrums = {}
     for item in electrums_data:
-        electrums.update({item["name"]:item["download_url"]})
+        if item["name"] not in ["deprecated"]:
+            electrums.update({item["name"]:item["download_url"]})
 
     lightwallets_data = get_github_folder_contents("KomodoPlatform", "coins", "light_wallet_d")
     lightwallets = {}
     for item in lightwallets_data:
-        lightwallets.update({item["name"]:item["download_url"]})
+        if item["name"] not in ["deprecated"]:
+            lightwallets.update({item["name"]:item["download_url"]})
 
     icons_data = get_github_folder_contents("KomodoPlatform", "coins", "icons")
     icons = {}
     for item in icons_data:
-        icons.update({item["name"]:item["download_url"]})
-
-    for coin in coins:
-        coin = handle_translate_coins(coin)
-        coins_data = pre_populate_coins_data(coins_data, coin)
+        if item["name"] not in ["deprecated"]:
+            icons.update({item["name"]:item["download_url"]})
 
     coins_data = get_coins_repo_lightwallets(lightwallets, coins_data)
     coins_data = get_coins_repo_electrums(electrums, coins_data)
@@ -333,27 +333,6 @@ def pre_populate_coins_data(coins_data, coin):
     if "mm2_compatible" not in coins_data[coin]:
         coins_data[coin].update({"mm2_compatible": 0})
 
-    coin = get_alt_name(coin)
-    if coin not in coins_data:
-        coins_data.update({coin: {}})
-    if "coins_info" not in coins_data[coin]:
-        coins_data[coin].update({"coins_info": {}})
-    if "dpow" not in coins_data[coin]:
-        coins_data[coin].update({"dpow": {}})
-    if "dpow_tenure" not in coins_data[coin]:
-        coins_data[coin].update({"dpow_tenure": {}})
-    if "dpow_active" not in coins_data[coin]:
-        coins_data[coin].update({"dpow_active": 0})
-    if "electrums" not in coins_data[coin]:
-        coins_data[coin].update({"electrums": []})
-    if "electrums_ssl" not in coins_data[coin]:
-        coins_data[coin].update({"electrums_ssl": []})
-    if "explorers" not in coins_data[coin]:
-        coins_data[coin].update({"explorers": []})
-    if "lightwallets" not in coins_data[coin]:
-        coins_data[coin].update({"lightwallets": []})
-    if "mm2_compatible" not in coins_data[coin]:
-        coins_data[coin].update({"mm2_compatible": 0})
     return coins_data
 
 
@@ -369,7 +348,6 @@ def remove_old_coins(coins_data):
 
 def update_coins(coins_data):
     for coin in coins_data:
-        logger.info(f"[update_coins] Updating {coin}")
         coin_data = coins_row()
         coin_data.coin = coin
         coin_data.coins_info = json.dumps(coins_data[coin]['coins_info'])
@@ -382,9 +360,3 @@ def update_coins(coins_data):
         coin_data.dpow_active = coins_data[coin]['dpow_active']
         coin_data.mm2_compatible = coins_data[coin]['mm2_compatible']
         coin_data.update()
-
-        coin = handle_translate_coins(coin)
-        if coin != coin_data.coin:
-            logger.warning(f"Creating second entry for {coin} / {coin_data.coin}")
-            coin_data.coin = coin
-            coin_data.update()
