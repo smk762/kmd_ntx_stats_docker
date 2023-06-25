4b59b78907c2d2a01eb042581631e82376087978
smk762 <smk762@iinet.net.au>
2023-06-25 21:48:55 +0800

    use latest mm2

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ docker-compose stop
 git pull
 docker-compose build
 docker-compose run web python3 manage.py migrate
-docker-compose run web python3 makemigrations 
+docker-compose run web python3 manage.py makemigrations 
 docker-compose up -d
 docker-compose logs -f --tail=100
 # TODO: include nginx / ssl
