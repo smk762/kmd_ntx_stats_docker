31646c4a6465e88fd09b5f56de428cc30822bae9
smk762 <smk762@iinet.net.au>
2023-07-08 18:21:25 +0800

    db env

--- mm2/view_nodes_table.sh
+++ mm2/view_nodes_table.sh
@@ -1,2 +1,1 @@
-DB='/home/komodian/mm2/DB/48008b497db7a423dcda9334afd61005f008f33e/MM2.db'
-sqlite3 $DB "SELECT * FROM nodes"
+sqlite3 ./mm2/DB/48008b497db7a423dcda9334afd61005f008f33e/MM2.db "SELECT * FROM nodes"
