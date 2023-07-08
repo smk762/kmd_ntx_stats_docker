30ad8d67bd8282907747854e255a772b9f80cbf9
smk762 <smk762@iinet.net.au>
2023-07-08 12:14:38 +0200

    add sqlite view scripts

---
+++ mm2/view_nodes_table.sh
@@ -0,0 +1,2 @@
+DB='/home/komodian/mm2/DB/48008b497db7a423dcda9334afd61005f008f33e/MM2.db'
+sqlite3 $DB "SELECT * FROM nodes"
---
+++ mm2/view_stats_nodes_table.sh
@@ -0,0 +1,1 @@
+sqlite3 /home/smk762/kmd_ntx_stats_docker/mm2/DB/48008b497db7a423dcda9334afd61005f008f33e/MM2.db "SELECT * FROM stats_nodes limit 10;"
