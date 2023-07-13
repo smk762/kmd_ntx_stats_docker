2e7130cbd41fa841cca93a5f1f334a418921c616
smk762 <smk762@iinet.net.au>
2023-07-13 22:42:14 +0800

    add cache folder

--- code/kmd_ntx_api/external_data.py
+++ code/kmd_ntx_api/external_data.py
@@ -24,15 +24,15 @@ SCRIPT_PATH = dirname(realpath(sys.argv[0]))
 
 # Unified coins config
 COINS_CONFIG_URL = "https://raw.githubusercontent.com/KomodoPlatform/coins/master/utils/coins_config.json"
-COINS_CONFIG_PATH = f"{SCRIPT_PATH}/coins_config.json"
+COINS_CONFIG_PATH = f"{SCRIPT_PATH}/cache/coins_config.json"
 VERSION_TIMESPANS = refresh_external_data(COINS_CONFIG_PATH, COINS_CONFIG_URL)
 
 # Seed node version epochs
 VERSION_TIMESPANS_URL = "https://raw.githubusercontent.com/KomodoPlatform/dPoW/seednode-update/doc/seed_version_epochs.json"
-VERSION_TIMESPANS_PATH = f"{SCRIPT_PATH}/seed_version_epochs.json"
+VERSION_TIMESPANS_PATH = f"{SCRIPT_PATH}/cache/seed_version_epochs.json"
 VERSION_TIMESPANS = refresh_external_data(VERSION_TIMESPANS_PATH, VERSION_TIMESPANS_URL)
 
 # Links to ecosystem sites
 ECOSYSTEM_LINKS_URL = "https://raw.githubusercontent.com/gcharang/data/master/info/ecosystem.json"
-ECOSYSTEM_LINKS_PATH = f"{SCRIPT_PATH}/ecosystem.json"
-ECOSYSTEM_LINKS = refresh_external_data(ECOSYSTEM_LINKS_URL, ECOSYSTEM_LINKS_PATH)
+ECOSYSTEM_LINKS_PATH = f"{SCRIPT_PATH}/cache/ecosystem.json"
+ECOSYSTEM_LINKS = refresh_external_data(ECOSYSTEM_LINKS_PATH, ECOSYSTEM_LINKS_URL)
